0370293464f311d9b227ff5dcb175744d43c11d7
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-05-23 13:10:48 -0400

    updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+"use strict";
+
+var rollDice = function(){
+	var d1 = Math.floor (Math.random ()* 6)+ 1;
+	var d2 = Math.floor (Math.random ()* 6)+ 1;
+	var diceTotal = d1 + d2;
+	$('.die1').html(d1);
+	$('.die2').html(d2);
+	$('.status').html('you rolled = ' + diceTotal + '.');
+}
+
+	$('.roll').click(function() {
+	rollDice();
+});
+
+var getNum = function () {
+sides = $('.dice-sides-input :selected').text();
+return Math.floor(Math.random() * sides) + 1
+}
+
+
+	

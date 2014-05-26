1347a190e290827c531181f87592fa147c982c07
Josh Date <date0010@me.com>
2014-05-26 00:50:56 -0400

    updating test parameters and building

--- test/spec/test.js
+++ test/spec/test.js
@@ -6,21 +6,29 @@
 	'use strict';
 
 	describe('test for the number of die', function (){
-		it ('should throw an error for non-number types', function (){
-			// this will test to ensure that the number entered for the die input is an actual number //
-			var nAnForm;
-			expect(nAnForm).to.throw(error);
+		// it ('should throw an error for non-number types', function (){
+		// 	// this will test to ensure that the number entered for the die input is an actual number //
+		// 	var nAnForm;
+		// 	expect(nAnForm).to.throw(error);
 
-		});
+		// });
 
 	it ('should create the correct number of input fields for die', function () {
 		// this will ensure that the quantity entered for the die is an actual number //
-		expect(2).to.equal($('.dice-quantity-input').length);
+		$('.dice-number-input').val(2);
+		diceSides();
+		expect(2).to.equal($('.dice-sides li').length);
 	})
 
 	it ('should check that the quantity entered is within the given range', function () {
 		//this will check to see if the quantity of dice entered is within the range of numbers set //
-		expect(dice).to.have.length.within(1,6);
+		$('.dice-number-input').val(1);
+		diceSides();
+		$('.dice-sides-input-1').val(6);
+		$('.selectRoll').click();
+		$('.rollButton').click();
+
+		expect($('.youRolled').html()).to.be.within(1,6);
 	})
 	});
 })();
@@ -30,36 +38,18 @@
 	'use strict';
 
 	describe('input quantity of die', function () {
-		it('should create the appropriate number of die', function (){
-			//instance of will assert that the target is an instance of constructor //
-			var Dice = function (name) { this.name = name; }
-  			, Die = new Diece('die');
-
-  			expect(Die).to.be.an.instanceof(dice);	
-		})
-	});
-})();
-
-
-(function () {
-	'use strict';
-
-	describe('input quantity of die', function () {
-		it('should drop down menu for die options', function (){
-			//instance of will assert that the target is an instance of constructor //
-			Die.prototype.dice = function(){};
-			expect(Die).to.respondTo('dice');	
-		})
 		
 		//this will check to see if the quantity of die entered is the appropriate number set //
 		it('should create the appropriate quantity of die', function () {
-			var diceQuantity;
-			expect(diceQuantity).to.equal(2);
-		})
-
-		it ('should update the variable entered', function () {
-			// this should update the variable entered with the appropriate length... not sure if this is correct//
-			expect('dice').to.have.length(2);
+			$('.dice-number-input').val(4);
+			diceSides();
+			$('.dice-sides-input-1').val(6);
+			$('.dice-sides-input-2').val(6);
+			$('.dice-sides-input-3').val(6);
+			$('.dice-sides-input-4').val(6);
+			$('.selectRoll').click();
+			$('.rollButton').click();
+			expect(4).to.equal($('.scene').length)
 		})
 	});
 
